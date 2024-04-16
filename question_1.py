17192195fef8268434d32ccac70536c34cefa471
MD SIFAT ALI <sifatali520@gmail.com>
2024-04-16 21:35:51 +0600

    Update question_1.py

--- question_1.py
+++ question_1.py
@@ -1,9 +1,14 @@
-alist = [23,59,25,56,3,65]
-
-big_number = 0
-
-for number in alist:
-    if number > big_number:
-        big_number = number
+def find_largest_number(numbers):
+    if not numbers:
+        return None
+    
+    big_number = 0
+    for number in numbers:
+        if number > big_number:
+            big_number = number
         
-print(big_number)
+    return big_number
+
+
+numbers = [10, 5, 20, 15, 30]
+print(find_largest_number(numbers))
