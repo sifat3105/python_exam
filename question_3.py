037bfa7efee332216523f159f95bd2bd79c8f93d
MD SIFAT ALI <sifatali520@gmail.com>
2024-04-16 22:13:16 +0600

    Update question_3.py

---
+++ question_3.py
@@ -0,0 +1,14 @@
+def fibonacci(n):
+    fib_sequence = []
+    if n >= 1:
+        fib_sequence.append(0)
+    if n >= 2:
+        fib_sequence.append(1)
+    for i in range(2, n):
+        fib_sequence.append(fib_sequence[i - 1] + fib_sequence[i - 2])
+    return fib_sequence
+
+# Example usage:
+n = 10
+print("First", n, "numbers in Fibonacci sequence:")
+print(fibonacci(n))
