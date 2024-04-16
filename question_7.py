8e7c4dde1df24551d52981a0e168ea805cb3898d
MD SIFAT ALI <sifatali520@gmail.com>
2024-04-16 21:40:32 +0600

    Create question_7.py

---
+++ question_7.py
@@ -0,0 +1,11 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        result = 1
+        for i in range(1, n + 1):
+            result *= i
+        return result
+
+n = 4
+print(factorial(n))
